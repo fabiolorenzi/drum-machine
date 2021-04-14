9b597ba84a0d5286f1352f058602481d4d73cdc2
fabiolorenzi <fabiolorenzi@gmail.com>
2021-04-14 10:51:27 +0100

    first commit

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainContainer() {
+    return(
+        <div className="mainContainer">
+            <h1>Hello World</h1>
+        </div>
+    );
+};
+
+export default MainContainer;
