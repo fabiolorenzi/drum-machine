fafd1bd96c9c4d2cb4ac0db6cd2e0e38bd27f662
fabiolorenzi <fabiolorenzi@gmail.com>
2021-04-15 11:12:36 +0100

    triplet button inserted

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -7,15 +7,34 @@ function MainContainer() {
     const [settings, setSettings] = useState({
         playing: false,
         bpm: 120,
-        mills: 500
+        triplet: false,
+        mills: 125
     });
 
     //--------------------FUNCTIONS--------------------
 
     const handleBpmChange = e => {
         e.preventDefault();
-        setSettings({...settings, bpm: e.target.value, mills: 60000 / settings.bpm});
-        console.log(settings);
+        setSettings({...settings, bpm: e.target.value, mills: 15000 / settings.bpm});
+    };
+
+    const starter = e => {
+        e.preventDefault();
+        setSettings({...settings, playing: true});
+    };
+
+    const stopper = e => {
+        e.preventDefault();
+        setSettings({...settings, playing: false});
+    };
+
+    const tripletSetter = e => {
+        e.preventDefault();
+        if (settings.triplet) {
+            setSettings({...settings, triplet: false, mills: 20000 / settings.bpm});
+        } else {
+            setSettings({...settings, triplet: true, mills: 15000 / settings.bpm});
+        };
     };
 
     //--------------------RETURN--------------------
@@ -27,16 +46,21 @@ function MainContainer() {
                 <h2>by Fabio Lorenzi</h2>
             </div>
             <div className="playerSection">
-                <label htmlFor="bpm">BPM</label>
-                <input
-                    type="number"
-                    id="bpmSel"
-                    name="bpm"
-                    min="40"
-                    max="300"
-                    value={settings.bpm}
-                    onChange={handleBpmChange}
-                />
+                <div className="bpmSection">
+                    <label htmlFor="bpm">BPM</label>
+                    <input
+                        type="number"
+                        id="bpmSel"
+                        name="bpm"
+                        min="40"
+                        max="300"
+                        value={settings.bpm}
+                        onChange={handleBpmChange}
+                    />
+                </div>
+                <div className="secondLine">
+                    <button className="tripletButton" id={settings.triplet ? "tripletActive" : ""} onClick={tripletSetter}>3</button> 
+                </div>
             </div>
         </div>
     );
